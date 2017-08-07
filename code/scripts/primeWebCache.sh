32c1de3cc3c0e18ffedd8e811011bf627bdcfb94
Robert Warren <warren@tiny.local>
2017-08-07 12:26:14 -0400

    Small script to prime cache after data loads

--- code/scripts/primeWebCache.sh
+++ code/scripts/primeWebCache.sh
@@ -7,9 +7,20 @@ MAX_QUERIES=10
 #
 #
 #
+
+(
+  # Wait for lock on /var/lock/.myscript.exclusivelock (fd 200) for 10 seconds
+  flock -x -w 10 200 || exit 1
+
+  # Do stuff
+
+) 200>/var/lock/.myscript.exclusivelock
+
 grep "GET /sparql?query=" /var/log/apache2/access.log | cut -d "?" -f 2- | cut -d " " -f 1 | sort | uniq -c | sort -k 1 -n -r | head - -n ${MAX_QUERIES} | cut -d " " -f 2- > /tmp/queries.$$
 IFS=\n
 for QUERY in `cat /tmp/queries.$$`
 do
  curl "http://localhost/parql?${QUERY}" > /dev/null
 done
+
+# util-linux
