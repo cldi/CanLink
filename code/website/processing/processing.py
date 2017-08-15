475d06e0413722628b9a81e3b16e47c00c0ea772
Maharsh Patel <patel@maharsh.net>
2017-08-15 11:43:18 -0600

    updated project path

--- code/website/processing/processing.py
+++ code/website/processing/processing.py
@@ -31,8 +31,9 @@ SCHEMA = Namespace("http://schema.org/")
 FRBR = Namespace("http://purl.org/vocab/frbr/core#")
 CWRC = Namespace("http://sparql.cwrc.ca/ontologies/cwrc#")
 
-# processing_folder_prefix = "/home/ubuntu/CanLink/code/website/processing"      # for the server
-processing_folder_prefix = "/Users/maharshmellow/Google Drive/Code/Github/CanLink/code/website/processing"      # for local development
+# project_folder_path = "/home/ubuntu/CanLink/code"      # for the server
+project_folder_path = "/Users/maharshmellow/Google Drive/Code/Github/CanLink/code"      # for local development
+
 
 
 class Thesis():
@@ -506,7 +507,6 @@ class Thesis():
         # abstract
         if self.abstracts:
             for item in self.abstracts:
-                # TODO get the language of the abstract here
                 abstract_language = detect(item)
                 g.add((URIRef(self.uri), BIBO.abstract, Literal(item, lang=abstract_language)))
         # publisher
@@ -644,7 +644,7 @@ def saveErrorFile(content, silent_output):
     if silent_output: return None
     # error_file_name = hashlib.md5(str(content).encode("utf-8")).hexdigest() + ".mrc"
     error_file_name = hashlib.md5(str(time.time() + random.randrange(10000)).encode("utf-8")).hexdigest() + ".mrc"
-    with open(processing_folder_prefix + "/errors/"+error_file_name, "wb") as error_file:
+    with open(project_folder_path + "/website/processing/errors/"+error_file_name, "wb") as error_file:
         error_file.write(content)
 
     return error_file_name
@@ -692,13 +692,13 @@ def process(records_file, lac_upload, silent_output):
     errors = []
     submissions = []
 
-    with open(processing_folder_prefix + "/files/universities.pickle", "rb") as handle:
+    with open(project_folder_path + "/website/processing/files/universities.pickle", "rb") as handle:
         universities_dbpedia = pickle.load(handle)      # key: name, value: uri
 
-    with open(processing_folder_prefix + "/files/subjects.pickle", "rb") as handle:
+    with open(project_folder_path + "/website/processing/files/subjects.pickle", "rb") as handle:
         subjects = pickle.load(handle)      # key: subject name, value: uri
 
-    with open(processing_folder_prefix + "/files/degrees.pickle", "rb") as handle:
+    with open(project_folder_path + "/website/processing/files/degrees.pickle", "rb") as handle:
         degrees = pickle.load(handle)
     # used to keep non-persistent memory of the universities we have processed before
     # so that we don't need to go to dbpedia every time for the same file
@@ -818,7 +818,7 @@ def process(records_file, lac_upload, silent_output):
     # print(g.serialize(format="xml").decode("utf-8"))
     if len(submissions) > 0:
         output_file_name = hashlib.md5(str(time.time() + random.randrange(10000)).encode("utf-8")).hexdigest() + ".xml"
-        g.serialize(processing_folder_prefix + "/tmp/" + output_file_name, format="xml")
+        g.serialize(project_folder_path + "/website/processing/tmp/" + output_file_name, format="xml")
 
     # send the tweet
     if len(universities) > 0:
